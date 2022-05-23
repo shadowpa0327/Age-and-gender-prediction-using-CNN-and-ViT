8e7364dade0e640105ab715cc8a904f6d5013509
shadowpa0327 <shadowpa0327@gmail.com>
2022-05-23 15:25:05 +0000

    add drop path in ViT

--- model.py
+++ model.py
@@ -11,9 +11,9 @@ class ResNet_For_Age(nn.Module):
         self.num_features = self.resnet.num_features
         
         self.age_fc = nn.Sequential(
-                nn.Linear(in_features = self.num_features, out_features = 96),
+                nn.Linear(in_features = self.num_features, out_features = 256),
                 nn.ReLU(),
-                nn.Linear(in_features = 96, out_features = 1)
+                nn.Linear(in_features = 256, out_features = 1)
             )
 
         self.gender_fc = nn.Sequential(
@@ -36,22 +36,22 @@ class ResNet_For_Age(nn.Module):
 
 
 class Vit_For_Age(nn.Module):
-    def __init__(self, model_name='vit_small_patch16_224', pretrained = True):
+    def __init__(self, model_name='vit_small_patch16_224', pretrained = True, drop_path_rate = 0.05):
         super(Vit_For_Age, self).__init__()
 
-        self.vit = timm.create_model(model_name = model_name, pretrained = pretrained) 
+        self.vit = timm.create_model(model_name = model_name, pretrained = pretrained, drop_path_rate = drop_path_rate)  # tranformer model
         self.embed_dim = self.vit.embed_dim
 
         self.age_fc = nn.Sequential(
                 nn.Linear(in_features = self.embed_dim, out_features = 96),
                 nn.ReLU(),
-                nn.Linear(in_features = 96, out_features = 1)
-            )
+                nn.Linear(in_features = 96, out_features = 1) 
+            ) # regression
 
         self.gender_fc = nn.Sequential(
                 nn.Linear(in_features = self.embed_dim, out_features = 1),
                 nn.Sigmoid()
-            )
+            ) # binary classification
 
     def forward(self,x):
          x = self.vit.forward_features(x)
@@ -79,5 +79,7 @@ def build_model(model_name='resnet50', model_type = 'resnet' ,pretrained=True):
         model = ResNet_For_Age(model_name=model_name, pretrained=pretrained)
     elif model_type == 'transformer':
         model = Vit_For_Age(model_name=model_name, pretrained=pretrained)
+
+    print(model)
 
     return model
